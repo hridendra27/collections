dcf9406a60f11d11c6c38ed0ff4f0c2461ef5aaa
hridendra27 <hridendra27@gmail.com>
2018-03-19 14:02:09 +0530

    csssc

--- CollectionProgram/src/ArrayListEx/ArrayListEx.java
+++ CollectionProgram/src/ArrayListEx/ArrayListEx.java
@@ -40,12 +40,9 @@ for (String a : al11){
 		System.out.println("welcome");
 		System.out.println (all);
 		System.out.println("Commit");
-<<<<<<< HEAD
 	 //lo
-=======
 	 //lop1
 		//lokpi
->>>>>>> stash
 	}
 
 	}
